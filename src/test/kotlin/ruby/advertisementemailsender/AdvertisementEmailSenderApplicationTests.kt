9231635371853a3d5a40cb84540e4e813198fedc
Ruby <93859705+MisterRuby@users.noreply.github.com>
2024-12-05 07:12:43 +0900

    chore: 프로젝트 초기 셋팅

---
+++ src/test/kotlin/ruby/advertisementemailsender/AdvertisementEmailSenderApplicationTests.kt
@@ -0,0 +1,13 @@
+package ruby.advertisementemailsender
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvertisementEmailSenderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
